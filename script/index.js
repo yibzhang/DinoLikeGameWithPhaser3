968bedcfd4eb24bc1c443bf91c48617298f8b041
yibzhang <zhangyiboturku@gmail.com>
2021-01-05 10:40:27 +0200

    delete cameras

--- script/index.js
+++ script/index.js
@@ -16,19 +16,18 @@ let config = {
   }
 };
 
+let game = new Phaser.Game(config);
+
 let frame;
 let player;
-let enemy;
-let clouds;
-let mountains;
-let start_text;
-let score = 0;
-let score_text;
-let grass;
-
-let cursors;
-
-let game = new Phaser.Game(config);
+// let enemy;
+// let clouds;
+// let mountains;
+// let start_text;
+// let score = 0;
+// let score_text;
+// let grass;
+// let cursors;
 
 function preload() {
   this.load.image('frame', 'images/frame.png');
@@ -40,23 +39,26 @@ function preload() {
 }
 
 function create() {
-  this.physics.world.bounds.width = 10 * config.width;
-  this.physics.world.bounds.height = config.height;
-  this.cameras.main.setBounds(0, 0, this.physics.world.bounds.width, this.physics.world.bounds.height);
+  // this.physics.world.bounds.width = 10 * config.width;
+  // this.physics.world.bounds.height = config.height;
+  // this.physics.world.bounds.width = config.width;
+  // this.physics.world.bounds.height = config.height;
+  // this.cameras.main.setBounds(0, 0, this.physics.world.bounds.width, this.physics.world.bounds.height);
 
-  frame = this.add.image(0.5 * config.width, 0.5 * config.height, 'frame').setDisplaySize(config.width, config.height);
-  frame.setScrollFactor(0);
+  // frame = this.add.image(0.5 * config.width, 0.5 * config.height, 'frame').setDisplaySize(config.width, config.height);
+  // frame.setScrollFactor(0);
+  this.add.image(0, 0, 'frame');
 
   //Mountains and clouds added as Tile sprites to allow simple parallax
-  mountains = this.add.tileSprite(0.5 * config.width, 0.4 * config.height, 0, 0, 'mountains').setDisplaySize(config.width, 0.5 * config.height);
-  mountains.setScrollFactor(0);
-  clouds = this.add.tileSprite(0.5 * config.width, 0.2 * config.height, 0, 0, 'cloud').setDisplaySize(config.width, 0.3 * config.height);
-  clouds.setScrollFactor(0);
+  // mountains = this.add.tileSprite(0.5 * config.width, 0.4 * config.height, 0, 0, 'mountains').setDisplaySize(config.width, 0.5 * config.height);
+  // mountains.setScrollFactor(0);
+  // clouds = this.add.tileSprite(0.5 * config.width, 0.2 * config.height, 0, 0, 'cloud').setDisplaySize(config.width, 0.3 * config.height);
+  // clouds.setScrollFactor(0);
 
-  grass = this.physics.add.staticGroup();
+  // grass = this.physics.add.staticGroup();
 
   //Random grass
-  for (var i = 0; i < 20; i++) {
+  /*for (var i = 0; i < 20; i++) {
     let rndX = 0.5 + 0.01 * Phaser.Math.RND.between(0, 50);
     let rndY = 0.9 + 0.01 * Phaser.Math.RND.between(0, 10);
     let grassImage = 'grass-small';
@@ -66,16 +68,16 @@ function create() {
     }
 
     grass.create(0.5 * config.width * i * rndX, 0.8 * config.height * rndY, grassImage).setDisplaySize(0.1 * config.height, 0.1 * config.height);
-  }
+  }*/
 
-  player = this.physics.add.sprite(0.5 * config.width, config.height, 'player').setScale(0.25);
-  player.setCollideWorldBounds(true);
-  player.setBounce(0.1);
+  // player = this.physics.add.sprite(0.5 * config.width, config.height, 'player').setScale(0.25);
+  // player.setCollideWorldBounds(true);
+  // player.setBounce(0.1);
 
-  this.cameras.main.startFollow(player);
+  // this.cameras.main.startFollow(player);
 
   //  Our player animations, turning, walking left and walking right.
-  this.anims.create({
+  /*this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('player', { start: 7, end: 0 }),
     frameRate: 10,
@@ -93,18 +95,18 @@ function create() {
     frames: this.anims.generateFrameNumbers('player', { start: 0, end: 7 }),
     frameRate: 20,
     repeat: -1
-  });
+  });*/
 
   //this.physics.add.collider(player, )
 
-  cursors = this.input.keyboard.createCursorKeys();
+  // cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  mountains.tilePositionX = this.cameras.main.scrollX * 0.1;
-  clouds.tilePositionX = this.cameras.main.scrollX * 0.2;
+  // mountains.tilePositionX = this.cameras.main.scrollX * 0.1;
+  // clouds.tilePositionX = this.cameras.main.scrollX * 0.2;
 
-  if (cursors.left.isDown) {
+  /*if (cursors.left.isDown) {
     player.anims.play('left', true);
     player.setVelocityX(-150);
   }
@@ -119,5 +121,5 @@ function update() {
 
   if (cursors.up.isDown && player.body.blocked.down) {
     player.setVelocityY(-300);
-  }
+  }*/
 }

236f1f8ecd51e898ce50414b00aacf01b0d09e6a
yibzhang <zhangyiboturku@gmail.com>
2021-01-09 00:49:44 +0200

    delete initial example files

--- assets/Level.js
+++ assets/Level.js
@@ -8,19 +8,12 @@ class Level extends Phaser.Scene {
 	constructor() {
 		super("Level");
 		
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
 	}
 	
 	create() {
-		
-		// dino
-		this.add.image(400, 300, "dino");
-		
-		// text_1
-		const text_1 = this.add.text(400, 450);
-		text_1.setOrigin(0.5, 0);
-		text_1.text = "Welcome to Phaser Editor 2D!";
-		text_1.setStyle({"fontSize":"30px","fontStyle":"bold"});
-		
 	}
 	
 	/* START-USER-CODE */

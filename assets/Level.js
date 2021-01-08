29b50949cf0347bb0a10bf2f9739bf6442aeb871
yibzhang <zhangyiboturku@gmail.com>
2021-01-09 01:09:14 +0200

    add mountain as tilesprite

--- assets/Level.js
+++ assets/Level.js
@@ -8,18 +8,28 @@ class Level extends Phaser.Scene {
 	constructor() {
 		super("Level");
 		
+		/** @type {Phaser.GameObjects.TileSprite} */
+		this.mountains;
+		
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
 	
 	create() {
+		
+		// mountains
+		const mountains = this.add.tileSprite(407, 221, 819, 231, "mountains");
+		
+		this.mountains = mountains;
 	}
 	
 	/* START-USER-CODE */
 
 	// Write your code here.
-
+	update(){
+		this.mountains.tilePositionX -= 1;
+	}
 	/* END-USER-CODE */
 }
 
